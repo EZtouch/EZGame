3f6f14f682aba3d105de7c2f75178e9a34bb6a45
EZtouch <hashem.jameel@gmail.com>
2018-03-22 19:09:11 +0300

    Hopefully this stops the CI build for gh-pages branch

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -26,6 +26,6 @@ exec("git init");
 exec("git add .");
 exec(`git config user.name "${ghName}"`);
 exec(`git config user.email "${ghEmail}"`);
-exec('git commit -m "docs(docs): update gh-pages"');
+exec('git commit -m "[skip ci] docs(docs): update gh-pages"');
 exec(`git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`);
 echo("Docs deployed!!");
